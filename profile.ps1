e2a42e87d868f567aedaf2a399459912d10dd91a
Jean-François Le Foll <jeff.lefoll@outlook.fr>
2018-04-15 10:32:48 +0200

    profile avec get-childitemcolor

---
+++ profile.ps1
@@ -0,0 +1,29 @@
+﻿function prompt {
+    $origLastExitCode = $LastExitCode
+
+    Write-Host "$env:COMPUTERNAME" -NoNewline -ForegroundColor DarkYellow
+    Write-Host " in " -NoNewline -ForegroundColor White
+
+    $curPath = $ExecutionContext.SessionState.Path.CurrentLocation.Path
+    if ($curPath.ToLower().StartsWith($Home.ToLower()))
+    {
+        $curPath = "~" + $curPath.SubString($Home.Length)
+    } 
+
+    Write-Host $curPath -NoNewline -ForegroundColor Blue
+
+    Write-VcsStatus
+
+    $LASTEXITCODE = $origLastExitCode
+    "$('>' * ($nestedPromptLevel + 1)) "
+}
+
+
+
+Import-Module posh-git
+Import-Module Get-ChildItemColor
+
+Set-Alias l Get-ChildItemColor -option AllScope
+Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
+
+$global:GitPromptSettings.BeforeText = ' on ['
