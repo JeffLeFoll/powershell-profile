79c573e684af0c0750cc089894243aa514a64f47
Jean-François Le Foll <jeff.lefoll@outlook.fr>
2018-04-16 15:48:46 +0200

    shell et exemple

---
+++ profile_avec_PS-Color.ps1
@@ -0,0 +1,26 @@
+﻿function prompt {
+    $origLastExitCode = $LastExitCode
+
+    Write-Host "$env:COMPUTERNAME" -NoNewline -ForegroundColor DarkYellow
+    Write-Host " in " -NoNewline -ForegroundColor White
+
+    $curPath = $ExecutionContext.SessionState.Path.CurrentLocation.Path
+    if ($curPath.ToLower().StartsWith($Home.ToLower()))
+    {
+        $curPath = "~" + $curPath.SubString($Home.Length)
+    } 
+
+    Write-Host $curPath -NoNewline -ForegroundColor Blue
+
+    Write-VcsStatus
+
+    $LASTEXITCODE = $origLastExitCode
+    "$('>' * ($nestedPromptLevel + 1)) "
+}
+
+
+
+Import-Module posh-git
+Import-Module PSColor
+
+$global:GitPromptSettings.BeforeText = ' on ['
